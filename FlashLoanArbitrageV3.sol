73751e187149e0cc513595d93905482a0a8bb2f7
Upshorelabs <dexexchange2025@gmail.com>
2026-03-12 00:02:16 +0300

    Update FlashLoanArbitrageV3.sol

--- FlashLoanArbitrageV3.sol
+++ FlashLoanArbitrageV3.sol
@@ -67,17 +67,13 @@ contract FlashLoanArbitrageV3{
         bytes calldata params
     ) external{
 
-        (
-            address router1,
-            address router2,
-            address tokenA,
-            address tokenB
-        ) = abi.decode(params,(address,address,address,address));
+        (address router1,address router2,address tokenA,address tokenB) =
+        abi.decode(params,(address,address,address,address));
 
-        address;
+        address ; // fixed: declare path array
 
+        // First swap: tokenA -> tokenB
         IERC20(tokenA).approve(router1,amount);
-
         path[0] = tokenA;
         path[1] = tokenB;
 
@@ -91,8 +87,8 @@ contract FlashLoanArbitrageV3{
 
         uint tokenBBalance = IERC20(tokenB).balanceOf(address(this));
 
+        // Second swap: tokenB -> tokenA
         IERC20(tokenB).approve(router2,tokenBBalance);
-
         path[0] = tokenB;
         path[1] = tokenA;
 
@@ -104,33 +100,25 @@ contract FlashLoanArbitrageV3{
             block.timestamp
         );
 
+        // Repay flash loan
         uint totalDebt = amount + fee;
-
         IERC20(tokenA).approve(msg.sender,totalDebt);
 
+        // Send profit to profitWallet
         uint balanceAfter = IERC20(tokenA).balanceOf(address(this));
-
         if(balanceAfter > totalDebt){
-
             uint profit = balanceAfter - totalDebt;
-
             IERC20(tokenA).transfer(profitWallet,profit);
-
         }
     }
 
     function withdrawToken(address token) external onlyOwner{
-
         uint balance = IERC20(token).balanceOf(address(this));
-
         IERC20(token).transfer(profitWallet,balance);
-
     }
 
     function updateProfitWallet(address newWallet) external onlyOwner{
-
         profitWallet = newWallet;
-
     }
 
 }
